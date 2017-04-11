66c05b35d8b9238c4b36aee1e0da5814b0bc2291
Eibhlin McGeady <eibhlinmcg93@gmail.com>
2017-04-11 16:37:02 +0100

    add s

--- src/js/api/MakeUpApiClient.js
+++ src/js/api/MakeUpApiClient.js
@@ -2,7 +2,7 @@
 
 import 'whatwg-fetch';
 
-const BASE_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json';
+const BASE_URL = 'https://makeup-api.herokuapp.com/api/v1/products.json';
 
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
